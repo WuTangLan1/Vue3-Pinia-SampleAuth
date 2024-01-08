ee7162f415f41e937a13c57269caa4c14e6a72f1
WuTangLan1 <finnmassari16@gmail.com>
2024-01-08 22:26:19 +0200

    Working wsith reg+login disop dinner break

--- src/views/registerView.vue
+++ src/views/registerView.vue
@@ -1,13 +1,53 @@
 <script>
 import RegModal from '@/components/Authdir/RegModal.vue';
+
 export default {
-    name : 'register-View',
-    components : {
-        RegModal
-    }
+  name: 'register-View',
+  components: {
+    RegModal
+  }
 }
 </script>
 
 <template>
-    <RegModal/>
-</template>
+    <div class="register-view-container">
+      <div class="background-container">
+        <img src="@/assets/p1.png" class="background-image" />
+        <img src="@/assets/p2.png" class="background-image" />
+        <img src="@/assets/p3.png" class="background-image" />
+      </div>
+      <RegModal/>
+    </div>
+  </template>
+  
+
+  <style scoped>
+.register-view-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.background-container {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+
+.background-image {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0.5; /* Adjust as needed */
+}
+
+/* Ensure RegModal is on top of the background images */
+.reg-modal {
+  position: relative;
+  z-index: 2;
+}
+</style>
